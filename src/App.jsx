c357cfb4713f65e74f9205f4ce4d4de0cb2040bb
sumitabha4 <sumitabha4@gmail.com>
2026-02-06 15:02:49 +0000

    search section imaplemented with static data

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from '@/components/Header'
 import Marquee from '@/components/Marquee'
 import Hero from '@/components/Hero'
 import Features from '@/components/Features'
+import SeacrhSection from '@/components/SearchSection'
 
 function App() {
     return (
@@ -14,6 +15,7 @@ function App() {
                 <Marquee />
                 <Hero />
                 <Features />
+                <SeacrhSection />
             </div>
         </>
     )
